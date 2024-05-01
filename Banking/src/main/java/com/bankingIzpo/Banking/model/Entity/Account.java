e1d791caa46070d0f5681609d06b47cd401ab6f2
Izpo17F <ponce.1714.frp@gmail.com>
2024-05-01 16:33:18 -0500

    creacion de las tablas

--- Banking/src/main/java/com/bankingIzpo/Banking/model/Entity/Account.java
+++ Banking/src/main/java/com/bankingIzpo/Banking/model/Entity/Account.java
@@ -1,4 +1,33 @@
 package com.bankingIzpo.Banking.model.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "accounts")
+
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_number", unique = true, nullable = false)
+    private String accountNumber;
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+    @Column(name = "owwner_name", nullable = false)
+    private String ownnerName;
+    @Column(name = "ownner_email", nullable = false)
+    private String ownnerEmail;
+    @Column(name="create_at")
+    private LocalDate createAt;
+    @Column(name="update_at")
+    private LocalDate updateAt;
 }
